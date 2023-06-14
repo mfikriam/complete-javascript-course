662ce985c6b91b71725734a2bc5d2afb5c6f9e28
mfikriam <yapkun17@gmail.com>
2023-06-14 19:07:36 +0800

    10. Practice Assignments

---
+++ 01-Fundamentals-Part-1/assigments.js
@@ -0,0 +1,7 @@
+//**********************************
+//* LECTURE: Values and Variables
+let country = 'Indonesia';
+let continent = 'Sulawesi';
+let population = 273.8;
+
+console.log(country, continent, population);

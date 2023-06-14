c273290347c75e9c25b01bcb11446cf137700c5f
mfikriam <yapkun17@gmail.com>
2023-06-14 19:54:21 +0800

    12. let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -29,7 +29,6 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
-*/
 
 //**********************************
 //* Data Types
@@ -52,3 +51,18 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+//**********************************
+//* let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);

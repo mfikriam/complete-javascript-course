8cce69473bfb207d77ab5c732047df658c03f718
mfikriam <yapkun17@gmail.com>
2023-06-15 19:11:13 +0800

    20. Type Conversion and Coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -160,6 +160,27 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 */
+//**********************************
+//* Type Conversion and Coercion
+
+// ? type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// ? type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //! Coding Challenge #1
@@ -217,7 +238,7 @@ console.log(BMIMark, BMIJohn, markHigherBMI);
 
 ? GOOD LUCK 😀
 */
-
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -237,3 +258,4 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
 }
+*/

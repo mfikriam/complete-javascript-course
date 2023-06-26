253f5e6738a407b5d692620a4aba22911e2a1ef6
mfikriam <yapkun17@gmail.com>
2023-06-26 22:59:36 +0800

    32. Activating Strict Mode

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,6 +1,15 @@
+'use strict';
+
 //**********************************
-//* Linking a JavaScript File
-// ? Comparison operators
+//* Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //! Coding Challenge #5

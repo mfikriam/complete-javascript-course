621437ebb6fa530747d657abb6ce1d3b5074a3f7
mfikriam <yapkun17@gmail.com>
2023-07-10 21:09:15 +0800

    73. Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -2,6 +2,15 @@
 //************** HEADING 1 **************/
 // ? SUB HEADING
 
-//************** HEADING 2 **************/
-//************** HEADING 3 **************/
-//************** HEADING 4 **************/
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});

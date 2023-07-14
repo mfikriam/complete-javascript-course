c24320cdb037ba2abce8539cd6c5ca38959d2748
mfikriam <yapkun17@gmail.com>
2023-07-14 12:10:24 +0800

    79. PROJECT #2: Modal Window

---
+++ 06-Modal/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//************** VARIABLES **************/
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);

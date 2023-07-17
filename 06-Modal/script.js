9438bc78e18279effebec60d33fa0471ff1875b9
mfikriam <yapkun17@gmail.com>
2023-07-17 17:51:15 +0800

    80. Working With Classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,4 +5,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+
+//************** FUNCTIONS **************/
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//************** BUTTONS SHOW MODAL EVENT LISTENERS **************/
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+//************** CLOSE MODAL EVENT LISTENERS **************/
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);

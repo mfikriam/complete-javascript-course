3c403daa7debc6b3b6e709d8694cd0ab7873072f
mfikriam <yapkun17@gmail.com>
2023-07-19 20:50:33 +0800

    97. The this Keyword in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -77,3 +77,40 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+
+// console.log(this);
+
+const calcAge2 = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+
+calcAge2(1991);
+
+const calcAge2Arrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+
+calcAge2Arrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+
+f();

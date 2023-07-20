d7e21a0ed9c0f613b97daef096cd3520f4ab9548
mfikriam <yapkun17@gmail.com>
2023-07-20 21:43:08 +0800

    99. Primitives vs. Objects

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -115,7 +115,7 @@ matilda.calcAge();
 const f = jonas.calcAge;
 
 f();
-*/
+
 
 var firstName = 'Matilda';
 
@@ -163,3 +163,19 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+*/
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me:', me);
